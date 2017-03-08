5114541652bad372d3dc7005866ac590e0066d0e
Kelsey Hightower <kelsey.hightower@gmail.com>
2017-03-08 15:32:18 -0800

    updating sutff

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
 kubectl delete deployments gif-maker --context dev
 kubectl delete deployments gif-maker --context khightower
 kubectl delete deployments gif-maker --context production
+git tag -d 0.0.1
+git push --delete origin 0.0.1
